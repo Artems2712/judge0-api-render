f2aa147f9810b58ae5cf2ecfe48de4fb1d827c7d
Artems2712 <144522226+Artems2712@users.noreply.github.com>
2025-05-18 18:02:00 +0400

    Create puma.rb

---
+++ config/puma.rb
@@ -0,0 +1,11 @@
+# config/puma.rb  (замена оригиналу)
+max  = Integer(ENV.fetch('RAILS_MAX_THREADS', 5))
+min  = Integer(ENV.fetch('RAILS_MIN_THREADS', max))
+threads min, max
+
+port        Integer(ENV.fetch('PORT', 10000))
+environment ENV.fetch('RAILS_ENV', 'production')
+
+workers Integer(ENV.fetch('WEB_CONCURRENCY', 2))
+preload_app!
+plugin :tmp_restart
